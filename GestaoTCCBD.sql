79ab050c61fa4b4a6e97654a3dceeed7b5a608eb
Gabyy1 <100389136+Gabyy1@users.noreply.github.com>
2023-06-15 23:23:14 -0300

    c

--- GestaoTCCBD.sql
+++ GestaoTCCBD.sql
@@ -23,4 +23,16 @@ idPessoa INT NOT NULL
 Primary Key (numProfessor)
 Foreign Key (idPessoa) references pessoa(id)
 )
+Go
 
+Create Table aluno(
+ra  VARCHAR(13) NOT NULL,
+nome VARCHAR(100) NOT NULL,
+email VARCHAR(100) NOT NULL,
+tel VARCHAR(11) NOT  NULL,
+cpf VARCHAR(11) NOT NULL,
+curso VARCHAR(30) NOT NULL,
+idPessoa INT NOT NULL
+Primary Key (ra)
+Foreign Key (idPessoa) references pessoa(id)
+)
